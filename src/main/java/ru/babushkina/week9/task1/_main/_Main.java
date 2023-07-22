5770a7a187bd4e14f84d9086f60851962ef293ba
Bloody-Mary <37978402+Bloody-Mary@users.noreply.github.com>
2023-07-22 22:42:37 +0300

    Updated classes _Main and Animal

--- src/main/java/ru/babushkina/week9/task1/_main/_Main.java
+++ src/main/java/ru/babushkina/week9/task1/_main/_Main.java
@@ -1,4 +1,70 @@
 package ru.babushkina.week9.task1._main;
 
+import ru.babushkina.week9.task1.model.Animal;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class _Main {
+    public static void main(String[] args) {
+        List<Animal> animals = new ArrayList<>();
+        animals.add(new Animal("cow", 1));
+        animals.add(new Animal("pig", 2));
+        animals.add(new Animal("cat", 3));
+        animals.add(new Animal("dog", 4));
+        animals.add(new Animal("duck", 5));
+        animals.add(new Animal("tiger", 6));
+        animals.add(new Animal("zebra", 7));
+        animals.add(new Animal("elephant", 8));
+        animals.add(new Animal("hippo", 9));
+        animals.add(new Animal("horse", 10));
+
+//        Optional<Animal> lion = animals.stream()
+//                .filter(animal -> animal.getName().equalsIgnoreCase("lion"))
+//                .findFirst();
+//        if (lion.isPresent()) {
+//            System.out.println("Возраст льва : " + lion.get().getAge());;
+//        } else {
+//            throw new IllegalArgumentException("Лев не найден");
+//        }
+
+//        animals.stream()
+//                .sorted(Comparator.comparing(Animal::getAge))
+//                .forEach(animal -> System.out.println("Имя: " + animal.getName() + ", возраст: " + animal.getAge()));
+//    }
+
+//        long count = animals.stream()
+//                .filter(animal -> animal.getAge() > 4)
+//                .count();
+//        System.out.println("Животные, у которых возраст больше 4 лет: " + count);
+//    }
+
+        Map<String, Animal> animalMap = animals.stream()
+                .collect(Collectors.toMap(Animal::getName, animal -> animal));
+        animalMap.forEach((name, animal) -> System.out.println("Ключ: " + name + ", значение: " + animal.getAge()));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //        String animalNames = animals.stream()
+//                .map(Animal::getName)
+//                .reduce((acc, name) -> acc + ", " + name)
+//                .orElse("");
+//        System.out.println("Названия животных: " + animalNames);
+//
+//    }
 }
--- src/main/java/ru/babushkina/week9/task1/model/Animal.java
+++ src/main/java/ru/babushkina/week9/task1/model/Animal.java
@@ -1,4 +1,19 @@
 package ru.babushkina.week9.task1.model;
 
 public class Animal {
+    private final String name;
+    private final int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
